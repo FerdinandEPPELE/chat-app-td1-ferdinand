0754ce15f9ff6b000c8b1a51ab5a376c16695116
eppele211 <feppele@gmail.com>
2024-01-25 10:59:01 +0100

    feat: added the list of message and a send button to enable the user to push their message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 
 <template>
-  <div>navbar</div>
-  <router-view></router-view>
+  <div class="bg-slate-600 h-full text-white">
+    <router-view></router-view>
+  </div>
 </template>
--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,5 +1,23 @@
+<script setup>
+import {ref} from 'vue';
+const messageContent = ref('');
+const messages = ref([]);
+
+const addMessage = () => {
+    messages.value.push(messageContent.value)
+    messageContent.value = ''
+}
+</script>
+
 <template>
-    <div class="bg-cyan-600">
-        Salut !
+    <div class="pl-4">
+        <div v-for="(message, index) in messages" :key="index">
+            {{ message }}
+        </div>
     </div>
+    <div class="flex p-4">
+        <textarea v-model="messageContent" name="message" id="message" rows="1" class="text-black"></textarea>
+        <button @click="addMessage" class="rounded-md bg-black p-3 ml-3">Envoyer</button>
+    </div>
+    
 </template>
